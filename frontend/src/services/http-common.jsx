0b3d3c30893ce949927dbfa65121b6986697f013
Görkem <grrarr.1907@gmail.com>
2024-12-26 15:22:39 +0300

    fix

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://full-web-three.vercel.app/api",
+  baseURL: "https://full-web-three.vercel.app",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
